661a5816817e90d9b5f48b764048f658f0f5cae6
Kerim Satirli <k.satirli@gmail.com>
2022-09-26 16:31:54 +0200

    bumps Terraform to `1.3.0` and GitHub Provider to `5.3.0`

--- terraform.tf
+++ terraform.tf
@@ -12,13 +12,13 @@ terraform {
 
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.1.0/
+    # see https://registry.terraform.io/providers/integrations/github/5.3.0/
     github = {
       source  = "integrations/github"
-      version = ">= 5.1.0, < 6.0.0"
+      version = ">= 5.3.0, < 6.0.0"
     }
   }
 
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
-  required_version = ">= 1.2.9"
+  required_version = ">= 1.3.0"
 }
