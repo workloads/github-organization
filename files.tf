bff6782fae3cb1b647c005e5154476c55d70280c
rizkybiz <justin.defrank@hashicorp.com>
2023-01-19 09:02:39 -0500

    Added a first pass at the intention behind workloads

--- files.tf
+++ files.tf
@@ -1,7 +1,11 @@
 resource "github_repository_file" "organization_profile" {
-  repository          = module.special_repositories[".github"].github_repository.name
-  file                = "/profile/README.md"
-  branch              = "main"
-  content             = "test"
+  repository = module.special_repositories[".github"].github_repository.name
+  file       = "/profile/README.md"
+  branch     = "main"
+  content = templatefile(
+    "./templates/organization-profile.tftpl", {
+      background = file("./templates/background-brief.txt")
+    }
+  )
   overwrite_on_create = true
 }
