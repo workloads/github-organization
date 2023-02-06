9003377844e3cd5f428c0cf93c7f5551955f1168
Kerim Satirli <k.satirli@gmail.com>
2023-02-06 10:08:32 +0100

    bumps GitHub Provider from `5.16.0` to `5.17.0`

--- terraform.tf
+++ terraform.tf
@@ -12,10 +12,10 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.16.0/
+    # see https://registry.terraform.io/providers/integrations/github/5.17.0/
     github = {
       source  = "integrations/github"
-      version = ">= 5.16.0, < 6.0.0"
+      version = ">= 5.17.0, < 6.0.0"
     }
   }
 
