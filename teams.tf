97ce362c7f7fa810ec9e28a6dafa76d202cc9f1a
Kerim Satirli <k.satirli@gmail.com>
2023-04-07 10:57:49 +0200

    adds review request policy for `maintainers`

--- teams.tf
+++ teams.tf
@@ -8,6 +8,17 @@ resource "github_team" "maintainers" {
   privacy                   = "closed"
 }
 
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/team_settings
+resource "github_team_settings" "maintainers" {
+  team_id = github_team.maintainers.id
+
+  review_request_delegation {
+    algorithm    = "LOAD_BALANCE"
+    member_count = 1
+    notify       = true
+  }
+}
+
 # add all members of `var.organization_owners` to the `maintainers` team
 # see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/team_membership
 resource "github_team_membership" "maintainers" {
