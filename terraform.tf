a6939c5e6cf9309858f2fc5105205a2507ccf06e
Kerim Satirli <k.satirli@gmail.com>
2023-05-02 15:05:25 +0200

    updates Lock File with hashes for additional operating systems

--- terraform.tf
+++ terraform.tf
@@ -12,10 +12,10 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.23.0/
+    # see https://registry.terraform.io/providers/integrations/github/5.24.0/
     github = {
       source  = "integrations/github"
-      version = ">= 5.23.0, < 6.0.0"
+      version = ">= 5.24.0, < 6.0.0"
     }
   }
 
