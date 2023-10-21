46fe3009728a4b79760d38963f02005a9a16b680
Kerim Satirli <k.satirli@gmail.com>
2023-10-21 17:04:20 +0200

    bumps providers and GHA versions

--- terraform.tf
+++ terraform.tf
@@ -14,10 +14,10 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.39.0/
+    # see https://registry.terraform.io/providers/integrations/github/5.40.0/
     github = {
       source  = "integrations/github"
-      version = ">= 5.39.0, < 6.0.0"
+      version = ">= 5.40.0, < 6.0.0"
     }
   }
 
